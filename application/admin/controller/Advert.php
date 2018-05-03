4ce898c46d96c7c415c85f1a2c7319098a8cc1dc
user.name <1079493871@qq.com>
2018-05-03 16:26:52 +0800

    yyc

--- application/admin/controller/Advert.php
+++ application/admin/controller/Advert.php
@@ -2,18 +2,90 @@
 namespace app\admin\Controller;
 use think\Controller;
 use think\Request;   
-
+use think\Db;
 class Advert extends Controller
 {   
+        //广告添加
+    public function advert_add()
+    {   
+         if(request::instance()->isGet()){ 
+            return view('advert_add');
+        }
+        
+        if(request::instance()->isPost()){
+            $advert = input('post.');
+            $file   = request()->file('ad_img');
+            $info   = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+         if($info){
+            // 成功上传后 获取上传信息
+            $advert['ad_img'] = $info->getSaveName();
+            $res = DB::table('ad')->insert($advert);
+            if($res)
+            {
+                $this->redirect('Advert/advert_list');
+            }else{
+                $this->redirect('Advert/advert_add');
+            }
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+             }
+        }
+        
+    }
     // 广告列表
     public function advert_list()
     {
+        $page  = input('get.page');
+        $count = DB::table('ad')->count();
+        $res   = Db::table('ad')->paginate(5,$count);
+        $page_res  = $count-1;
+        $page_size = isset($page)?$page:1;
+        $this->assign('page_size',$page_size);
+        $this->assign('page_res',$page_res);
+        $this->assign('count',$count);
+        $this->assign('res',$res);
         return view('advert_list');
     }
-    //广告添加
-    public function advert_add()
-    {
-        return view('advert_add');
-    }
 
-}
+    public function del(){
+        $id      = input('get.id');//获取商品id
+        $ad_data = Db::table('ad')->where(['ad_id'=>$id])->find();  
+        $old_img = './uploads/'.$ad_data['ad_img'];
+        $res     = Db::table('ad')->delete($id);
+        if($res){  
+            unlink($old_img);
+           $this->redirect('Advert/advert_list');
+         }else
+         {  
+           return $this->error("删除失败",url('Advert/del'));
+        }
+    }
+    public function up(){
+        $id     = input('get.id');
+        $up_res = Db::table('ad')->where('ad_id',$id)->find();
+        return $this->fetch('advert_updata',['res'=>$up_res]);
+    }
+    public function updatas(){
+        $ad_id  = input("post.ad_id");
+        $old_img = './uploads/'.input("post.old_img");
+        $post   = input('post.');
+        unset($post['old_img']);
+        $file   = request()->file('ad_img');
+        $info   = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+        if($info)
+        {
+            $post['ad_img'] = $info->getSaveName();
+            $upd  = Db::table('ad')->where(['ad_id'=>$ad_id])->update($post);
+             if($upd)
+            {
+                unlink($old_img);
+                return $this->success('成功',url('advert_list'));
+            }else{
+                return $this->error('失败',url('up'));
+            }
+        }   
+    }
+‘【；票【】票【票【【品牌【破咯偶【扑街【雑
+
+} 
---
+++ application/admin/controller/Payment.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\admin\Controller;
+use think\Controller;
+use think\Request;   
+use think\Db;
+class Payment extends Controller
+{
+	public function payment_add()
+	{
+		if(request::instance()->isGet()){ 
+            return view('payment_add');
+        }
+
+        if(request::instance()->isPost())
+        {
+        	$post = input('post.');
+        	$pay_insert = Db::table('payment')->insert($post);
+        	
+        	if($pay_insert)
+        	{
+        		$this->redirect('payment_list');
+        	}else{
+        		$this->redirect('payment_add');
+        	}
+        }
+		
+	}
+
+	public function payment_list()
+	{
+		$pay_sel = Db::table('payment')->order('pay_order','desc')->select();
+		$this->assign('pay_sel',$pay_sel);
+		return view('payment_list');
+	}
+	public function payment_del(){
+		   $pay_id = $_GET['pay_id'];//获取支付id 
+		   $res = Db::table('payment')->where("pay_id= $pay_id")->delete();  
+		   if($res){  
+		      echo 1;  
+		   }else{  
+		      echo 2;
+		      }
+		      echo json_encode($pay_id);
+	}
+
+}
